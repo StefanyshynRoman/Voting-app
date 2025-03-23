824c857f2746e3b947f2d70d776ce5bfa21e5a46
roman <romanpz051@gmail.com>
2025-03-23 13:56:49 +0100

    0.0.0.3-post

---
+++ votingapp/src/main/java/com/voting/votingapp/services/PollService.java
@@ -0,0 +1,18 @@
+package com.voting.votingapp.services;
+
+import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.repositories.PollRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PollService {
+    private final PollRepository pollRepository;
+
+    public PollService(PollRepository pollRepository) {
+        this.pollRepository = pollRepository;
+    }
+
+    public Poll createPoll(Poll poll) {
+        return pollRepository.save(poll);
+    }
+}
