37239ee972a057751105edd883a8cab34a57d8e5
roman <romanpz051@gmail.com>
2025-03-29 17:17:02 +0100

    0.0.0.4-back_vote

--- votingapp/src/main/java/com/voting/votingapp/controllers/PollController.java
+++ votingapp/src/main/java/com/voting/votingapp/controllers/PollController.java
@@ -1,11 +1,12 @@
 package com.voting.votingapp.controllers;
 
 import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.request.Vote;
 import com.voting.votingapp.services.PollService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/polls")
@@ -21,5 +22,23 @@ public class PollController {
     @PostMapping
     public Poll createPoll(@RequestBody Poll poll) {
         return pollService.createPoll(poll);
+    }
+
+    @GetMapping
+    public List<Poll> getAllPolls() {
+        return pollService.getAllPolls();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getAllPoll(@PathVariable Long id) {
+        return pollService.getPollById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity
+                        .notFound().build());
+    }
+
+    @PostMapping("/vote")
+    public void vote(@RequestBody Vote vote) {
+         pollService.vote(vote.getPollId(), vote.getOptionIndex());
     }
 }
--- votingapp/src/main/java/com/voting/votingapp/services/PollService.java
+++ votingapp/src/main/java/com/voting/votingapp/services/PollService.java
@@ -1,8 +1,13 @@
 package com.voting.votingapp.services;
 
+import com.voting.votingapp.model.OptionVote;
 import com.voting.votingapp.model.Poll;
 import com.voting.votingapp.repositories.PollRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollService {
@@ -14,5 +19,30 @@ public class PollService {
 
     public Poll createPoll(Poll poll) {
         return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPollById(Long id) {
+        return pollRepository.findById(id);
+    }
+
+    public void vote(Long pollId, int optionIndex) {
+        //get Poll from DB
+        Poll poll = pollRepository.findById(pollId).orElseThrow(() -> new RuntimeException("Poll not found"));
+        //get All Options
+        List<OptionVote> options = poll.getOptions();
+        //if index for vote is not valid, throw error
+        if (optionIndex < 0 || optionIndex >= options.size()) {
+            throw new IllegalArgumentException("Invalid option index");
+        }
+        //get selected Option
+        OptionVote selectedOption = options.get(optionIndex);
+        //increment vote for selected option
+        selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+        //sav incremented option into the DB
+        pollRepository.save(poll);
     }
 }
